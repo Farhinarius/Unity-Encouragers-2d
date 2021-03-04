0084aacf1e86147b617b7813d4a374e8f6c986f4
ScuM <lalka727@yandex.ru>
2021-03-05 01:45:24 +0300

    Fix DoubleDamage Collider on hands

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -5,33 +5,52 @@ using UnityEngine;
 public class Hand : MonoBehaviour
 {
     public PlayerController player;
-
+    private BoxCollider2D handCollider;
+    private bool handAttackCheck;
+    private float iFrameTime;
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponentInParent<PlayerController>();
+        handCollider = GetComponent<BoxCollider2D>();
+        handAttackCheck = false;
+        iFrameTime = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (handCollider.enabled == false)
+        {
+            if (iFrameTime >= 0.01f)
+            {
+                handCollider.enabled = true;
+            }
+            else if (iFrameTime < 0.01f)
+            {
+                iFrameTime += Time.deltaTime;
+            }
+        }
     }
 
-    // Need to fix DoubleDamageCollider!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if ( (HandsController.rightIsCalm == false) || (HandsController.leftIsCalm == false) )
         {
             if (other.tag == "Enemy")
             {
+
                 other.GetComponent<DamageableEnemy>().Damage(1);
-                other.GetComponent<Rigidbody2D>().AddForce(player.LookDirection * 1000, ForceMode2D.Impulse);
+                handCollider.enabled = false;
+                iFrameTime = 0f;
+                other.GetComponent<Rigidbody2D>().AddForce(player.LookDirection * 3000, ForceMode2D.Impulse);
 
             }
             else if (other.tag == "Item")
             {
                 other.GetComponent<Destructible>().Damage();
+                handCollider.enabled = false;
+                iFrameTime = 0f;
                 other.GetComponent<Rigidbody2D>().AddForce(player.LookDirection*1000, ForceMode2D.Impulse);
 
 
