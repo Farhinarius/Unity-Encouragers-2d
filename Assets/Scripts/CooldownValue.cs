e1aa7ef4508518d0ec407c10454932968b5b4180
Farhinarius <farhat3027@gmail.com>
2021-03-05 01:50:10 +0300

    Added mana bar functionality

--- Assets/Scripts/CooldownValue.cs
+++ Assets/Scripts/CooldownValue.cs
@@ -6,14 +6,8 @@ using UnityEngine.UI;
 public class CooldownValue: MonoBehaviour
 {
     public Text[] cdValues;     // where text is gameObject;
-
-
-    void Start()
+    private void FixedUpdate() 
     {
-
-    }
-
-    private void FixedUpdate() {
         cdValues[0].text = MageController.staticController.getLightningCooldown.ToString();
         cdValues[1].text = MageController.staticController.getSpellCircleCooldown.ToString();
         cdValues[2].text = MageController.staticController.getDefenseCooldown.ToString();
